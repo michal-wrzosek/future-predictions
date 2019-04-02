d527186d28828c2343b58611d43a0ca80566ef16
Michał Wrzosek <michal@wrzosek.pl>
2019-04-02 12:31:43 +0200

    Updates

---
+++ precache-manifest.696c5b5b7e6c6422d74469cbf46151d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38734ff628ffe3b60cb0",
+    "url": "/future-predictions/static/js/runtime~main.0cb14907.js"
+  },
+  {
+    "revision": "314066969617bab3e77d",
+    "url": "/future-predictions/static/js/main.3900ee0d.chunk.js"
+  },
+  {
+    "revision": "01372b14583ed31d5662",
+    "url": "/future-predictions/static/js/2.cc2798fb.chunk.js"
+  },
+  {
+    "revision": "314066969617bab3e77d",
+    "url": "/future-predictions/static/css/main.54bacd24.chunk.css"
+  },
+  {
+    "revision": "bfa5321dbf55d047548995292f9994e1",
+    "url": "/future-predictions/index.html"
+  }
+];
