f403f1724baa98333145888a1c5f3c44a0c10344
Michał Wrzosek <michal@wrzosek.pl>
2019-04-06 20:32:14 +0200

    Updates

--- precache-manifest.53a8355933eb85f84c15f2ef2069af23.js
+++ precache-manifest.53a8355933eb85f84c15f2ef2069af23.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/future-predictions/static/js/runtime~main.0cb14907.js"
   },
   {
-    "revision": "96d5e7f69d2c534c590a",
-    "url": "/future-predictions/static/js/main.1c4505eb.chunk.js"
+    "revision": "76c3752413e034609cbc",
+    "url": "/future-predictions/static/js/main.20a8e4c4.chunk.js"
   },
   {
     "revision": "a693c7873f6b99109731",
     "url": "/future-predictions/static/js/2.c5b2a793.chunk.js"
   },
   {
-    "revision": "96d5e7f69d2c534c590a",
+    "revision": "76c3752413e034609cbc",
     "url": "/future-predictions/static/css/main.54bacd24.chunk.css"
   },
   {
-    "revision": "d9f66010d8d5b7d0a06a4aaa99501468",
+    "revision": "a979d8709cac30b4a2865e95faacccfd",
     "url": "/future-predictions/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/future-predictions/precache-manifest.e3b5f7b218277b6b2e1ac55af4156b92.js"
+  "/future-predictions/precache-manifest.53a8355933eb85f84c15f2ef2069af23.js"
 );
 
 workbox.clientsClaim();
